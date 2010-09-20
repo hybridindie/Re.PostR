3f03274d02097c33c6b215467af6b006110ffce8
John Brien <iam@hybridindie.com>
2010-09-20 11:41:38 -0400

    started galleries

---
+++ app/models/gallery.rb
@@ -0,0 +1,20 @@
+class Gallery < Post
+  has_many :photos
+  # Add function to create title on file upload with a humanized file name
+  #    Validation added in the mean time
+  def title
+    post_data[:title]
+  end
+  
+  def title=(value)
+    self.post_data[:title] = value
+  end
+  
+  def description
+    post_data[:description]
+  end
+  
+  def description=(value)
+    self.post_data[:description] = value
+  end
+end
